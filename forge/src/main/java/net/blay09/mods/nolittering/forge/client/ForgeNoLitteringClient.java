c5e7bdcf18a0fd2c1e0753b5e9952d154e840458
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-12-15 13:09:35 +0100

    feat: Update to Forge 1.21.11

--- forge/src/main/java/net/blay09/mods/nolittering/forge/client/ForgeNoLitteringClient.java
+++ forge/src/main/java/net/blay09/mods/nolittering/forge/client/ForgeNoLitteringClient.java
@@ -1,6 +1,6 @@
 package net.blay09.mods.nolittering.forge.client;
 
-import net.blay09.mods.balm.api.client.BalmClient;
+import net.blay09.mods.balm.client.BalmClient;
 import net.blay09.mods.nolittering.client.NoLitteringClient;
 
 public class ForgeNoLitteringClient {
