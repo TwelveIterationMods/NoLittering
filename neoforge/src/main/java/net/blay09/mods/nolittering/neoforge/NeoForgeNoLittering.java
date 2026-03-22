2dfff010826beaf23f7e4600568710999133a400
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-03-22 15:53:38 +0100

    feat: Update to Minecraft 26.1

--- neoforge/src/main/java/net/blay09/mods/nolittering/neoforge/NeoForgeNoLittering.java
+++ neoforge/src/main/java/net/blay09/mods/nolittering/neoforge/NeoForgeNoLittering.java
@@ -4,13 +4,14 @@ import net.blay09.mods.balm.Balm;
 import net.blay09.mods.balm.neoforge.platform.runtime.NeoForgeLoadContext;
 import net.blay09.mods.nolittering.NoLittering;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 
 @Mod(NoLittering.MOD_ID)
 public class NeoForgeNoLittering {
 
-    public NeoForgeNoLittering(IEventBus modEventBus) {
-        final var context = new NeoForgeLoadContext(modEventBus);
+    public NeoForgeNoLittering(ModContainer modContainer, IEventBus modEventBus) {
+        final var context = new NeoForgeLoadContext(modContainer, modEventBus);
         Balm.initializeMod(NoLittering.MOD_ID, context, new NoLittering());
     }
 }
